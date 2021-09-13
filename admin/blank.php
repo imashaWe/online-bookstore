65216dc5d3db6162eb0d1b19176d2126b6222718
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-13 13:05:06 +0530

    integrate admin panel design

---
+++ admin/blank.php
@@ -0,0 +1,17 @@
+<?php
+require_once('header.php');
+?>
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+
+
+
+        </div>
+    </main>
+<?php
+require_once('footer.php');
+?>
