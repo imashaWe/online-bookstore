a1152a1b16f6b4ee5750a571a6cc3275abad9301
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-15 10:57:17 +0530

    integrate online db

--- admin/db.php
+++ admin/db.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "online-bookstore-db.mysql.database.azure.com";
+$username = "dev@online-bookstore-db";
+$password = "Debuggers@18";
 $database = "online-bookstore";
 
 // create database connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-// check database connection's errors
+// check whether database connection has errors
 if ($conn->connect_errno) die("Database connection error:" . $conn->connect_error);
