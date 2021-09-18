472c6485f545d7c0461c1d5f87acc950df0abe0b
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-18 13:05:03 +0530

    implemented edit method

--- admin/book-category-add.php
+++ admin/book-category-add.php
@@ -1,22 +1,42 @@
 <?php
 require("db.php");
-
+$category = "";
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM book_category WHERE id = {$id}";
+    $category = $conn->query($sql)->fetch_array()['category'];
+    $sql = "SELECT * FROM book_sub_category WHERE category_id = {$id}";
+    $sub_category_rows = $conn->query($sql);
+}
 if (isset($_POST['submit'])) {
     $category = $_POST['category'];
     if (empty($category)) {
         $error = "Please enter Category";
     } else {
-        $sql = "INSERT INTO book_category (category) VALUES('{$category}')";
-        $res = $conn->query($sql);
+        if (isset($_POST['id'])) { // update record
+            $id = $_POST['id'];
+            $sql = "UPDATE book_category SET category = '{$category}' WHERE id = {$id}";
+            $res = $conn->query($sql);
+            $sql = " DELETE FROM book_sub_category WHERE category_id = {$id}";
+            $res = $conn->query($sql);
+            $insert_id = $id;
+        } else { // add new record
+            $sql = "INSERT INTO book_category (category) VALUES('{$category}')";
+            $res = $conn->query($sql);
+            $insert_id = $conn->insert_id;
+        }
         if ($res) {
             if (isset($_POST['sub_categories'])) {
-                $insert_id = $conn->insert_id;
                 $sql = "INSERT INTO book_sub_category (category_id,sub_category) VALUES (?,?)";
                 $stmt = $conn->prepare($sql);
+
                 $stmt->bind_param("is", $insert_id, $sub_category);
+
                 foreach ($_POST['sub_categories'] as $sub_category) {
                     $stmt->execute();
+
                 }
+                var_dump($stmt->error);
                 $stmt->close();
             }
             header("location:book-category.php");
@@ -24,18 +44,17 @@ if (isset($_POST['submit'])) {
             $error = "Database error";
         }
 
+
     }
 }
-
-
 ?>
 <?php require_once('header.php'); ?>
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Add New Category</h1>
+        <h1 class="mt-4"><?=isset($_POST['id'])?'Add New':'Edit'?>&nbsp;Category</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item">Book Category</li>
-            <li class="breadcrumb-item active">Add New Category</li>
+            <li class="breadcrumb-item active"><?=isset($_POST['id'])?'Add New':'Edit'?>&nbsp;Category</li>
         </ol>
 
         <div class="row justify-content-center">
@@ -62,7 +81,7 @@ if (isset($_POST['submit'])) {
                                     <label class="form-label">Category Name:</label>
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" name="category">
+                                    <input type="text" class="form-control" name="category" value="<?= $category ?>">
                                 </div>
 
                             </div>
@@ -88,14 +107,33 @@ if (isset($_POST['submit'])) {
                                         </button>
                                     </div>
                                     <ul class="list-group" id="subCategoriesList">
-
+                                        <?php while (isset($_GET['id']) && $row = $sub_category_rows->fetch_array()): ?>
+                                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                <?= $row['sub_category'] ?>
+                                                <input type="hidden" name="sub_categories[]"
+                                                       value="<?= $row['sub_category'] ?>">
+                                                <span class="clickable list-item-delete text-danger"
+                                                      onclick="deleteElement(this);"><icon
+                                                            class="fa fa-trash"></icon></span>
+                                            </li>
+                                        <?php endwhile; ?>
                                     </ul>
                                 </div>
 
                             </div>
                             <br>
+                            <?php if (isset($_GET['id'])): ?>
+                                <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
+                            <?php endif; ?>
+                            <div class="row justify-content-end pb-2">
+                                <div class="col-1">
+                                    <a href="book-category.php" class="btn btn-outline-secondary btn-lg float-end" name="submit">Cancel</a>
+                                </div>
+                                <div class="col-1">
+                                    <button type="submit" class="btn btn-success btn-lg" name="submit">Save</button>
+                                </div>
+                            </div>
 
-                            <button type="submit" class="btn btn-success float-end" name="submit">Save</button>
                         </form>
                     </div>
                 </div>
@@ -119,13 +157,6 @@ if (isset($_POST['submit'])) {
                         <span class="clickable list-item-delete text-danger" onclick="deleteElement(this);"><icon class="fa fa-trash"></icon></span>
                 </li>`;
             subCategoriesList.insertAdjacentHTML('beforeend', item);
-            // let itemDeleteBtn = document.getElementsByClassName('list-item-delete');
-            // for (let item of itemDeleteBtn) {
-            //     item.addEventListener('click', (e) => {
-            //         console.log(e.target.parentNode)
-            //         ;
-            //     })
-            // }
             subCategoryNameField.value = null;
         })
     })();
--- admin/book-category.php
+++ admin/book-category.php
@@ -1,13 +1,21 @@
 <?php
 require("db.php");
 
-$sql = "SELECT * FROM book_category WHERE is_delete = '0'";
+$sql = "SELECT category,book_category.id,
+        IFNULL(GROUP_CONCAT(sub_category),'N/A') AS 'sub_category' 
+        FROM book_category 
+        LEFT JOIN book_sub_category 
+        ON book_sub_category.category_id = book_category.id AND book_sub_category.is_delete = 0
+        WHERE book_category.is_delete = 0 
+        GROUP BY book_category.id
+        ";
 $categories = $conn->query($sql);
 
 if (isset($_POST['delete_submit'])) {
     $id = $_POST['id'];
-    $sql = "UPDATE book_category SET is_delete = 1 WHERE id = '{$id}'";
-    $res = $conn->query($sql);
+    $sql = "UPDATE book_category SET is_delete = 1 WHERE id = {$id};
+            UPDATE book_sub_category SET is_delete = 1 WHERE category_id = {$id}";
+    $res = $conn->multi_query($sql);
     if ($res) {
         header("location:{$_SERVER['REQUEST_URI']}");
     }
@@ -17,72 +25,80 @@ if (isset($_POST['delete_submit'])) {
 ?>
 
 <?php require_once('header.php'); ?>
-<div class="modal" tabindex="-1" id="deleteModal">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Delete</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>Are you sure you want to delete this category?</p>
-            </div>
-            <div class="modal-footer">
-                <form action="" method="post">
-                    <input type="hidden" id="delete_id" name="id">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-danger" name="delete_submit">Delete</button>
-                </form>
+    <div class="modal" tabindex="-1" id="deleteModal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Delete</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to delete this category?</p>
+                </div>
+                <div class="modal-footer">
+                    <form action="" method="post">
+                        <input type="hidden" id="delete_id" name="id">
+                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-danger" name="delete_submit">Delete</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<main>
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Book Categories</h1>
-        <ol class="breadcrumb mb-4">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Book Categories</h1>
+            <ol class="breadcrumb mb-4">
                 <li class="breadcrumb-item active">Book Categories</li>
-        </ol>
+            </ol>
+
+            <div class="d-grid  justify-content-end pb-2">
+                <a href="book-category-add.php" class="btn btn-dark">Add New Category</a>
+            </div>
 
             <div class="card">
                 <div class="card-body">
                     <table class="table table-striped">
-                    <thead>
+                        <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>CATEGORY</th>
-                        </tr><br>
-                    </thead>
-                    <tbody>
-                    <?php while ($category = $categories->fetch_array()): ?>
-                        <tr>
-                            <td><?= $category['id'] ?></td>
-                            <td><?= $category['category'] ?></td>
-                            <td>
-                                <button type="button"
-                                        class="btn btn-danger btn-user-delete"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#deleteModal"
-                                        onclick="setDelete(<?= $category['id'] ?>)">
-                                    Delete
-                                </button>
-                            </td>
-                        </tr><br>
-                    <?php endwhile; ?>
-                    </tbody>
-                </table>
+                            <th>Category</th>
+                            <th>Sub Categories</th>
+                            <th class="text-end">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php while ($category = $categories->fetch_array()): ?>
+                            <tr>
+                                <td><?= $category['category'] ?></td>
+                                <td><?= $category['sub_category'] ?></td>
+                                <td class="text-end">
+                                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                        <a href="book-category-add.php?id=<?=$category['id']?>" class="btn btn-primary">Edit</a>
+                                        <button type="button"
+                                                class="btn btn-danger btn-user-delete"
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#deleteModal"
+                                                onclick="setDelete(<?= $category['id'] ?>)">
+                                            Delete
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
+
         </div>
+    </main>
 
-    </div>
-</main>
-
-<script>
-    function setDelete(id) {
-        const delete_id = document.getElementById('delete_id');
-        delete_id.value = id;
-    }
-</script>
+    <script>
+        function setDelete(id) {
+            const delete_id = document.getElementById('delete_id');
+            delete_id.value = id;
+        }
+    </script>
 
 <?php require_once('footer.php'); ?>
