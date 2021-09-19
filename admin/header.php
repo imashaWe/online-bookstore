bd219ccf5ab9b5902f9e79e73aaaafe73804fee5
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-19 12:01:51 +0530

    refactor code

--- admin/header.php
+++ admin/header.php
@@ -18,7 +18,7 @@ require_once 'core/user.php';
 <body class="sb-nav-fixed">
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.html">Online Bookstore</a>
+    <a class="navbar-brand ps-3" href="index.php">Online Bookstore</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
@@ -50,7 +50,7 @@ require_once 'core/user.php';
                 <div class="nav">
 
                     <div class="sb-sidenav-menu-heading">Core</div>
-                    <a class="nav-link" href="index.html">
+                    <a class="nav-link" href="index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
                     </a>
