cebaf1600fdd6246fef1df81059cca2229f30440
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-19 12:20:29 +0530

    refactor code

--- admin/header.php
+++ admin/header.php
@@ -13,6 +13,7 @@ require_once 'core/user.php';
     <title>Dashboard - SB Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/main.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
 </head>
 <body class="sb-nav-fixed">
