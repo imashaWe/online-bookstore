5f00438df6d8fbfe716ed0eef59ddc368af8b513
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-28 22:07:01 +0530

    fix bug

--- core/config.php
+++ core/config.php
@@ -1,17 +1,17 @@
 <?php
 $_CONFIG = array(
-    'database' => array(
-        'servername' => "localhost",
-        'username' => "root",
-        'password' => "",
-        'database' => "online-bookstore",
-    ),
 //    'database' => array(
-//        'servername' => "online-bookstore.cruc6jn2s3ym.us-east-1.rds.amazonaws.com",
-//        'username' => "admin",
-//        'password' => "admin123",
+//        'servername' => "localhost",
+//        'username' => "root",
+//        'password' => "",
 //        'database' => "online-bookstore",
 //    ),
+    'database' => array(
+        'servername' => "online-bookstore.cruc6jn2s3ym.us-east-1.rds.amazonaws.com",
+        'username' => "admin",
+        'password' => "admin123",
+        'database' => "online-bookstore",
+    ),
     'AWS' => array(
         'key' => "AKIAWHMVJDKL543GRXKL",
         'secret' => "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",
