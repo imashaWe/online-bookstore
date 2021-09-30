6606eb0af90d9bb4b92ea7d2e92dba6917141792
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-10-01 00:18:04 +0530

    implemented mobile responsive

--- header.php
+++ header.php
@@ -15,6 +15,7 @@ if (!IS_LOGGED_IN) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#4173be" />
     <title>Book Berries</title>
     <link rel="shortcut icon" type="image/jpg" href="assets/images/favicon.png"/>
     <!-- bootstrap ccs -->
