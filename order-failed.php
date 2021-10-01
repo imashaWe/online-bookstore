3442962536a9df06cbbc88c7b9c57c94558cedea
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-10-01 21:57:33 +0530

    implemented mobile responsive

--- order-failed.php
+++ order-failed.php
@@ -1,8 +1,15 @@
 <?php require_once "header.php" ?>
+<?php require_once "header.php" ?>
 <main>
     <div class="container my-5">
-        <div class="d-flex justify-content-center">
-            <img src="assets/svgs/order-failed.svg" class="img-fluid" alt="">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="theme-text-heading text-danger">Oops,</h1>
+                <h3 class="theme-text">Something went wrong!</h3>
+            </div>
+            <div class="col-md-6">
+                <img src="assets/svgs/order-failed.svg" class="img-fluid" alt="">
+            </div>
         </div>
 
     </div>
--- order-placed.php
+++ order-placed.php
@@ -1,8 +1,14 @@
 <?php require_once "header.php" ?>
 <main>
     <div class="container my-5">
-        <div class="d-flex justify-content-center">
-            <img src="assets/svgs/order-placed.svg" class="img-fluid" alt="">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="theme-text-heading theme-primary-color">CONGRATULATIONS !!!</h1>
+                <h3 class="theme-text">Your order successfully placed.</h3>
+            </div>
+           <div class="col-md-6">
+               <img src="assets/svgs/order-placed.svg" class="img-fluid" alt="">
+           </div>
         </div>
 
     </div>
